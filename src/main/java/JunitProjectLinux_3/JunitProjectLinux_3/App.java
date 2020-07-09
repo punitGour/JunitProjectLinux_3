0cb56e90e88d49ee467b40c4109cb2a854e02c56
Poorvi Dharwad <poorvid@cybage.com>
2020-07-09 12:01:37 +0530

    commit-1, junit test and geckodriver

---
+++ src/main/java/JunitProjectLinux_3/JunitProjectLinux_3/App.java
@@ -0,0 +1,13 @@
+package JunitProjectLinux_3.JunitProjectLinux_3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
